ede6b05b490045d98e581d923e94542f3344d273
Edward "Teddy" Brow <edward.teddy.brow@gmail.com>
2015-05-04 10:55:24 -0700

    working with tables -- messy laptop code

--- drew_sys_tool/drew_sys_tool.py
+++ drew_sys_tool/drew_sys_tool.py
@@ -1,12 +1,18 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 from ui_mainscreen import Ui_MainScreen
+from classes import SystemState
 
+# create system state
+systemState = SystemState()
+systemState.debugAddWearable("Test Wearable", 666)
+
+# set up GUI application
 app = QApplication(sys.argv)
 
 window = QWidget()
 ui = Ui_MainScreen()
-ui.setupUi(window)
+ui.setupUi(window, systemState)
 window.show()
 
 sys.exit(app.exec_())
