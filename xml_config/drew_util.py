dae54e66639399dd5029f60d923b76dadde13dcb
wesshih <wesshih@uw.edu>
2015-05-03 21:54:42 -0700

    updated the xml to take out module and reflect more updated version

--- xml_config/drew_util.py
+++ xml_config/drew_util.py
@@ -1,41 +1,31 @@
+# variable dump objects
 
-class DREW_Object:
-  def __init__(self, name='drew_obj', xml_id=None):
+class Zone():
+  def __init__(self, name='default_zone', xml_id=None, hw_id=None, threshold=None):
     self.name = name
     self.xml_id = xml_id
-
-class Zone(DREW_Object):
-  def __init__(self, name='default_zone', xml_id=None, module=None, threshold=None, devices=None):
-    DREW_Object.__init__(self, name, xml_id)
-    self.module = module
+    self.hw_id = hw_id
     self.threshold = threshold
-    self.devices = devices
 
-class Module(DREW_Object):
-  def __init__(self, name='default_module', xml_id=None, hw_id=None):
-    DREW_Object.__init__(self, name, xml_id)
-    self.hw_id = hw_id
-
-class Device(DREW_Object):
-  def __init__(self, name='default_module', xml_id=None, mac=None, actions=None):
-    DREW_Object.__init__(self, name, xml_id)
+class Device():
+  def __init__(self, name='default_device', xml_id=None, mac=None, dev_type=0, enter=1, exit=0, zone=None):
+    self.name = name
+    self.xml_id = xml_id
     self.mac = mac
-    self.actions = actions
+    self.dev_type = dev_type
+    self.enter = enter
+    self.exit = exit
+    self.zone = zone
 
-class Wearable(DREW_Object):
-  def __init__(self, name='default_module', xml_id=None, hw_id=None):
-    DREW_Object.__init__(self, name, xml_id)
+class Wearable():
+  def __init__(self, name='default_wearable', xml_id=None, hw_id=None):
+    self.name = name
+    self.xml_id = xml_id
     self.hw_id = hw_id
 
-class Profile(DREW_Object):
-  def __init__(self, name='default_module', xml_id=None, wearables=None, zones=None):
-    DREW_Object.__init__(self, name, xml_id)
+class Profile():
+  def __init__(self, name='default_profile', xml_id=None, wearables=None, zones=None):
+    self.name = name
+    self.xml_id = xml_id
     self.wearables = wearables
     self.zones = zones
-
-#class Room(DREW_Object):
-#  def __init__(self, name='default_module', xml_id=None, threshold=None, zone=None, devices=None):
-#    DREW_Object.__init__(self, name, xml_id)
-#    self.threshold = threshold
-#    self.zone = zone
-#    self.devices = devices
--- xml_config/new_xml_reader.py
+++ xml_config/new_xml_reader.py
@@ -6,7 +6,6 @@ root = ET.parse('config_updated.xml').getroot()
 print("hello world")
 
 deviceDict = {}
-
 for d in root.find('devices'):
   print(d.tag, d.attrib)
   name = d.find('name').text
@@ -16,8 +15,18 @@ for d in root.find('devices'):
   for a in d.find('actions'):
     a_id = a.get('a_id')
     actions[a_id] = a.text
-  deviceDict[xml_id] = Device(name, xml_id, mac, actions)
+    deviceDict[xml_id] = Device(name, xml_id, mac, actions)
  
 print(deviceDict)
 print(deviceDict['d0'])
 
+class XML_Reader():
+  def __init__(self, root=None):
+    self.root = root
+    self.
+
+  def parse():
+    # parses the file from the root, but doesn't really return anything
+
+  def get_dict():
+    # gets an assoiated dictionary
