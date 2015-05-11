7e3c83ba4287803ab2c2f0e5d84e94b55f09ba01
Edward "Teddy" Brow <edward.teddy.brow@gmail.com>
2015-05-10 21:01:00 -0700

    just added a few comments

--- drew_sys_tool/drew_sys_tool.py
+++ drew_sys_tool/drew_sys_tool.py
@@ -6,12 +6,11 @@ from drew_util import SystemState
 # create system state
 systemState = SystemState("config_test.xml")
 
+# spawn other threads here
+
 # set up GUI application
 app = QApplication(sys.argv)
-
 uiControl = UiControl(systemState)
-
-# sys.exit(app.exec_())
-app.exec_()
+app.exec_() # blocks until application is closed
 
 systemState.xml.save()
