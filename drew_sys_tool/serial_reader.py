19c3b124b4c11baf3d3468d1b672ee29c9423e2e
Edward "Teddy" Brow <edward.teddy.brow@gmail.com>
2015-05-12 15:49:42 -0700

    missed a file -- adding now

--- drew_sys_tool/serial_reader.py
+++ drew_sys_tool/serial_reader.py
@@ -15,7 +15,10 @@ class SerialReader():
 
 			if (self.serialComm.inWaiting() > 0):
 				msg = SerialMessage(self.serialComm.readline().decode())
-				if (msg.msgType == MSG_TYPE_REG and not self.systemState.pause):
+				if (msg.msgType == MSG_TYPE_REG):
+					if (self.systemState.pause):
+						# if the system is paused, we throw out any msg besides "discovers"
+						continue
 					zone = self.systemState.getHardwareObjectByHwId(TID_Z, msg.zoneId)
 					wasInZone = self.systemState.zoneOccupation.lookup(msg.zoneId, msg.wearableId)
 					nowInZone = (msg.signalStrength - zone.threshold > 0)
@@ -28,5 +31,6 @@ class SerialReader():
 					self.systemState.wearableIds.add(msg.wearableId)
 				else: # (msg.msgType == MSG_TYPE_DISC_Z)
 					self.systemState.zoneIds.add(msg.zoneId)
+
 			else: # sleep a little bit if there's nothing to do
 				time.sleep(0.5)
